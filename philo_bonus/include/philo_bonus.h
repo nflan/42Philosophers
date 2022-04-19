88d6396620dea5f28d2729c063ba1aa5e9229918
Nicolas <nicolas.flan91@gmail.com>
2022-04-19 15:16:02 +0200

    initialisation des semaphores

--- philo_bonus/include/philo_bonus.h
+++ philo_bonus/include/philo_bonus.h
@@ -7,6 +7,7 @@
 # include <stdio.h>
 # include <unistd.h>
 # include <pthread.h>
+# include <semaphore.h>
 # include <sys/time.h>
 
 typedef struct s_phil
@@ -30,9 +31,7 @@ typedef struct s_all
 	int				died;
 	int				all_ate;
 	long long		first_timeval;
-	pthread_mutex_t	meal_check;
-	pthread_mutex_t	lock;
-	pthread_mutex_t	forks[300];
+	char			**forks;
 	t_phil			philo[300];
 }	t_all;
 
@@ -45,6 +44,11 @@ void		*ft_calloc(size_t nmemb, size_t size);
 int			ft_putstr_fd(char *s, int fd);
 long long	ft_atoi(const char *s);
 
+unsigned int	ft_strlen(const char *s1);
+char			*ft_strjoin(const char *s1, const char *s2);
+char			*ft_itoa(int nb);
+char			ft_pos(int i, int nb);
+int				ft_size_int(int nb);
 // PARSE
 int			ft_is_number(int ac, char **av, int error);
 int			ft_parsing(int ac, char **av, int error);
--- philo_bonus/srcs/main_bonus.c
+++ philo_bonus/srcs/main_bonus.c
@@ -6,11 +6,93 @@ int	ft_print_error(char *str)
 	return (1);
 }
 
+int	ft_init_philo(t_all *g)
+{
+	int	i;
+
+	i = 0;
+	while (i < g->nbphilo)
+	{
+		g->philo[i].id = i;
+		g->philo[i].x_ate = 0;
+		g->philo[i].left_fork_id = i;
+		g->philo[i].right_fork_id = (i + 1) % g->nbphilo;
+		g->philo[i].last_meal = 0;
+		g->philo[i].g = g;
+		i++;
+	}
+	return (0);
+}
+
+int	ft_free_all(t_all *g, int error)
+{
+	int	i;
+
+	i = 0;
+	if (g->forks)
+	{
+		if (g->forks[i])
+			while (g->forks[i] && i++)
+				free(g->forks[i]);
+		free(g->forks);
+	}
+	return (error);
+}
+
+int	ft_init_forks(t_all *g)
+{
+	int	i;
+
+	i = 0;
+	g->forks = NULL;
+	g->forks = ft_calloc(sizeof(char *), g->nbphilo - 1);
+	if (!g->forks)
+		return (1);
+	while (i++ < g->nbphilo)
+	{
+		g->forks[i] = ft_strjoin("fork_", ft_itoa(i));
+		if (!g->forks[i])
+			return (ft_free_all(g, 1));
+	}
+	return (0);
+}
+
+int	ft_init_all(t_all *g, char **av)
+{
+	g->nbphilo = ft_atoi(av[1]);
+	g->tdie = ft_atoi(av[2]);
+	g->teat = ft_atoi(av[3]);
+	g->tsleep = ft_atoi(av[4]);
+	g->died = 0;
+	g->all_ate = 0;
+	if (av[5])
+		g->nbeat = ft_atoi(av[5]);
+	else
+		g->nbeat = -1;
+	if (ft_init_forks(g))
+		return (1);
+	ft_init_philo(g);
+	return (0);
+}
+
+void	ft_print_all(t_all *g)
+{
+	int	i;
+
+	i = 0;
+	if (g->forks)
+		while (i++ < g->nbphilo)
+			printf("%s\n", g->forks[i]);
+}
+
 int	main(int ac, char **av)
 {
 	t_all	g;
 
 	if (ft_parsing(ac, av, 0))
 		return (ft_print_error("Parsing error"));
+	if (ft_init_all(&g, av))
+		return (1);
+	ft_free_all(&g, 0);
 	return (0);
 }
